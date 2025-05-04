f8f216783b912f0f156191747058e8eb61ba56e6
nndai <daihaha123zz@gmail.com>
2025-05-04 16:57:59 +0700

    Add OrderManager class

---
+++ src/OrderManager.h
@@ -0,0 +1,18 @@
+#ifndef ORDER_MANAGER_H_
+#define ORDER_MANAGER_H_
+
+#include "Order.h"
+#include "Database.h"
+#include <vector>
+
+class OrderManager {
+private:
+    Database* _db;
+
+public:
+    explicit OrderManager(Database* db);
+    void createOrder(const Order& order);
+    std::vector<Order> getAllOrders() const;
+};
+
+#endif
