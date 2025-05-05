b517621cb5f8e6a37afa2b9692ba134b1c9da403
nndai <daihaha123zz@gmail.com>
2025-05-05 22:23:48 +0700

    Remove commented-out menu display and input handling code from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,6 @@
 #include "AuthManager.h"
 #include "MenuManager.h"
 
-
-
-// void printMenu() {
-//     std::cout << "=============================\n";
-//     std::cout << "Music Store Management Menu:\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::CREATE_ORDER       << ". Create Order\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::FIND_MUSIC << ". Find Music\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::SOLD_ITEMS << ". Sold Items\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::ITEM_IN_STOCK << ". Items In Stock\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::SHOW_ALL_ITEMS << ". Show All Items\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::ADD_NEW_ITEM << ". Add New Item\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::EDIT_ITEM << ". Edit Item\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::REMOVE_ITEM << ". Remove Item\n";
-//     std::cout << (int)MenuActionFactory::MenuChoice::EXIT << ". Exit\n";
-//     std::cout << "=============================\n";
-//     std::cout << "Enter your choice: ";
-// }
-
 int main() {
 
     Database db;
@@ -43,36 +25,6 @@ int main() {
     MenuManager menuManager(&authManager, &store, &customerManager, &orderManager);
 
     menuManager.handleLoginMenu();
-
-    // int choice = 0;
-    // while (true) {
-    //     system("cls");
-    //     printMenu();
-    //     std::cin >> choice;
-
-    //     if (std::cin.fail()) {
-    //         std::cin.clear(); // clear error
-    //         std::cin.ignore(100, '\n'); // ignore until newline
-    //         std::cout << "Invalid input. Please enter a number.\n";
-    //         system("pause");
-    //         continue;
-    //     }
-
-    //     if ((int)MenuActionFactory::MenuChoice::EXIT == choice) {
-    //         std::cout << "Exiting program...\n";
-    //         break;
-    //     }
-
-    //     std::unique_ptr<IMenuAction> action(MenuActionFactory::createAction((MenuActionFactory::MenuChoice)choice, &store));
-
-    //     if (action) {
-    //         action->execute();
-    //     } else {
-    //         std::cout << "Invalid menu choice. Please try again.\n";
-    //     }
-
-    //     system("pause");
-    // }
 
     return 0;
 }
